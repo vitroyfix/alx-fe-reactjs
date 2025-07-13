a0b7a0a937d618e886181dbf8cf1e2ff6c791000
Peter Wangimwa <peterwangimwa@gmail.com>
2025-07-13 23:34:11 +0300

    Initial commit

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
+  );
+}
+
+export default App;
